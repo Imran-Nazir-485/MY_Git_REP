73a2965bdca2610a936dc6f8a2fbcd644b77fec1
Imran-Nazir-485 <imrnazir8@gmail.com>
2020-12-25 21:11:30 +0500

    seat management

--- airressystem/booked_seats.php
+++ airressystem/booked_seats.php
@@ -1,31 +1,238 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Booked Seats</title>
-</head>
-<body style="width: ">
 
-<h1   style="text-align: center;">This Is Working</h1 >
+
+
+	<title></title>
+
+<style type="text/css">
+	*{margin:0; padding: 0;box-sizing: border-box;}
+main{
+	border: 1px solid red; margin: 3% 20%;
+	}
+
+div
+{
+
+	border: 1px solid black;
+}
+h3{text-align: center;}
+</style>
+</head>
+<body>
+	<center>
+<input type="number" name="tickets"  id="t1" placeholder="Tickets"     style='width:100px;text-align:center;'    >
+<input type="submit" name="tickets"  id="t2" placeholder="Tickets"     style='width:100px;text-align:center;'    onclick="myfunt();"     >
+		
+	</center>
+
+
+<main>
+
+<h3 >Select Desired Seats</h3>
+<br><br><br><br>
+
+
+<center>
+<div>
+
+<br><br>
+
+
+<?php
+// 1- Connect to database
+$myconn= mysqli_connect("localhost","root","","airressystem");
+
+//2- Run SQL Query
+$myquery = "SELECT * FROM flightschedule";
+//3- Run Query
+$myresult = mysqli_query($myconn,$myquery);
+//4- Show results
+$rem=0;
+$booked=30;
+$myrow = mysqli_fetch_assoc($myresult);
+while($rem<=($myrow["tbseats"]-$booked))
+{
+echo "<input type=\"button\"   id=\"free\"   value=\"Business\" style=\"background-color: green;width: 50px\"; onclick=\"foo();background_color_change_function(this.value);\">"."&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp";
+$rem=$rem+1;
+if($rem%4==0)
+	{
+		echo "<br><br>";
+	}
+	// $rem=$rem+1;
+}
+echo "<br><br>";
+$rem=0;
+echo "<tr>";
+while($rem<=$booked)
+{
+echo  "<td>". "<input type=\"button\"  value=\"B$rem\" style=\"background-color: red;width: 50px\" ; onclick=\"myfunb()\">"."&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp";
+$rem=$rem+1;
+if($rem%4==0)
+	{
+		echo "<br><br>";
+	}
+	// $rem=$rem+1;
+}
+
+
+// echo "<br>";
+// $rem=0;
+// while($rem<=$myrow["rbseats"])
+// {echo "<input type=\"button\"  value=\"UnBooked\" style=\"background-color: white\">";
+// $rem=$rem+1;
+// if($rem%10==0)
+// 	{
+// 		echo "<br>";
+// 	}
+	
+// }
+	
+// echo "<input type=\"button\"  value=\"Booked\" style=\"background-color: red\">";
+// echo $myrow["rollno"] . "    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
+
+
+
+
+
+?>
+<br><br>
+</div>
+
+<br><br>
+
+
+<!-- <div>
+<?php
+// 1- Connect to database
+$myconn= mysqli_connect("localhost","root","","airressystem");
+
+//2- Run SQL Query
+$myquery = "SELECT * FROM flightschedule";
+//3- Run Query
+$myresult = mysqli_query($myconn,$myquery);
+//4- Show results
+$rem=0;
+$ebooked=20;
+$myrow = mysqli_fetch_assoc($myresult);
+while($rem<=($myrow["tbseats"]-$ebooked))
+{
+echo "<input type=\"button\"  value=\"E$rem\" style=\"background-color: green;width: 50px\"; >"."&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp";
+$rem=$rem+1;
+if($rem%4==0)
+	{
+		echo "<br><br>";
+	}
+	// $rem=$rem+1;
+}
+echo "<br><br>";
+$rem=0;
+while($rem<=$ebooked)
+{
+echo "<input type=\"button\"  value=\"E$rem\" style=\"background-color: red;width: 50px\"; >"."&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp";
+$rem=$rem+1;
+if($rem%4==0)
+	{
+		echo "<br><br>";
+	}
+	// $rem=$rem+1;
+}
+
+
+// echo "<br>";
+// $rem=0;
+// while($rem<=$myrow["rbseats"])
+// {echo "<input type=\"button\"  value=\"UnBooked\" style=\"background-color: white\">";
+// $rem=$rem+1;
+// if($rem%10==0)
+// 	{
+// 		echo "<br>";
+// 	}
+	
+// }
+	
+// echo "<input type=\"button\"  value=\"Booked\" style=\"background-color: red\">";
+// echo $myrow["rollno"] . "    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
+
+
+
+
+
+?>
+	
+</div>
+	 -->
+</center>
+
+
+	
+
+ 
+
+</main>
+
+
+
+
+
+
+
+
+<script type="text/javascript">
+
+	function myfunt()
+	{
+		 v2=document.getElementById("t1").value;
+        alert(v2);
+        counter=0;
+	}
+
+
+
+	// function myfun()
+	// {
+	// 	alert("Too many Selections");
+	// }
+
+
+
+
+
+function foo() {
+
+    // if( typeof foo.counter == 'undefined' ) {
+    //     foo.counter = 0;
+    // }
+    // foo.counter++;
+//alert("Too many Selections");
+counter=counter+1;
+  if (counter>v2) {alert("Too many Selections");}
+}
+
+
+
+
+
+
+
+
+
+function myfunb()
+	{
+		alert("This is Booked");
+	}
+
+function background_color_change_function(str) {
+
+if (str) {
+ document.getElementById("free").style.backgroundColor = "red"; }}
+
+</script>
+
+
+
+
 </body>
 </html>
 
@@ -36,7 +243,6 @@
 
 
 
-<?php
 
 
 
@@ -48,33 +254,8 @@
 
 
 
-include 'dbconn.php';
 
-$count=0;
-$myquery = "SELECT * FROM flightschedule";
-$myresult = mysqli_query($myconn,$myquery);
-echo "<center>";
-echo "<table border='1' width='300' style=\"border-collapse:collapse; \">  ";
-echo "<tr> ";
-echo "<td>FlightId</td>  ";
-echo "<td>DeptCity</td>  ";
-echo "<td>DestCity</td>  ";
-echo "<td>DepartTime</td>  ";
-echo "<td>DeptDate</td>  ";
-echo "<td>EconomyClass</td>  ";
-echo "<td>BusinessClass</td>  ";
-echo "<td>EconomyPrice</td>  ";
-echo "<td>BusinessPrice</td>  ";
-echo "<td>ReturnDate</td>  ";
-echo "<td>EconomyPrice</td>  ";
-echo "<td>BusinessPrice</td>  ";
-echo "<td>FlightDuration</td>  ";
-echo "<td colspan='2'>Operation</td>  ";
 
-echo "</tr> ";
-echo "<tr>";
-while($myrow = mysqli_fetch_assoc($myresult)){
-	
 
 
 
@@ -89,206 +270,3 @@ while($myrow = mysqli_fetch_assoc($myresult)){
 
 
 
-  echo   "<td>".$myrow['fid']."</td>"."<td>".$myrow['depcity']."</td>"." <td>".$myrow['destcity']."</td>"."<td>".$myrow['deptime']."</td>"."<td>".$myrow['depdate']."</td>"."<td>".$myrow['reseats']."</td>"."<td>".$myrow['rbseats']."</td>"."<td>".$myrow['ecprice']."</td>"."<td>".$myrow['bcprice']."</td>"."
-      <td>".$myrow['returndate']."</td>"."<td>".$myrow['reecprice']."</td>"."<td>".$myrow['rebcprice']."</td>"."<td>".$myrow['flightduration']."</td>"."<td>"."Oneway"."<input type='radio'   name='SELECT' value='SELECT' onclick=\"location.href='admin1.php?fid=$myrow[fid]'\"  >"."</td>"."<td>"."Return"."<input type='radio'   name='SELECT' value='SELECT' onclick=\"location.href='admin1.php?fid=$myrow[fid]'\"  >"."</td>";
-echo "</tr>";
-
-}
-
-
-echo "</table>";
-echo "</center>";
-
-
-
-
-// <td><a href='admin1.php?fid=$myrow[fid]'>SELECT</a></td></td>
-
-
-
-
-
-
-
-
-
-
-
-
-  ?>
-
-
-
-
-
-
-
-
-
-
-<!-- <?php
-// 1- Connect to database
-$myconn= mysqli_connect("localhost","root","","airressystem");
-
-//2- Run SQL Query
-$myquery = "SELECT * FROM flightschedule";
-//3- Run Query
-$myresult = mysqli_query($myconn,$myquery);
-//4- Show results
-$rem=0;
-while($myrow = mysqli_fetch_assoc($myresult)){
-while($rem<=($myrow["tbseats"]-$myrow["rbseats"]))
-{
-echo "<input type=\"button\"  value=\"Booked\" style=\"background-color: red\" width:\" 30px\">";
-$rem=$rem+1;
-if($rem%10==0)
-	{
-		echo "<br>";
-	}
-	// $rem=$rem+1;
-}
-echo "<br>";
-$rem=0;
-while($rem<=$myrow["rbseats"])
-{echo "<input type=\"button\"  value=\"UnBooked\" style=\"background-color: white\">";
-$rem=$rem+1;
-if($rem%9==0)
-	{
-		echo "<br>";
-	}
-	
-}
-	
-// echo "<input type=\"button\"  value=\"Booked\" style=\"background-color: red\">";
-// echo $myrow["rollno"] . "    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
-
-
-
-
-}
-?> -->
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<!-- <!DOCTYPE html>
-<html>
-<head>
-	<title>PHP Working</title>
-</head>
-<body>
-
-<form method="POST">
-	Enter Your Favorite Color:
-	<input type="text" name="favcolor">
-<input type="submit" name="submit"  value="Check">
-
-</form>
-
-<p>
-	
-	<?php
-if(isset($_POST['submit']))
-{
-     $favColor= $_POST['favcolor'];
-     //echo $favColor;
-switch ($favColor) {
-	case 'blue':
-		echo "<input type=\"button\"  value=\"Booked\" style=\"background-color: red\">";
-		break;
-	case 'black':
-		echo "Your Favorite Coloris {$favColor}";
-		break;
-	case 'red':
-		echo "Your Favorite Coloris {$favColor}";
-		break;
-	
-	default:
-		# code...
-		break;
-}
-
-
-
-
-}
-
-	?>
-</p>
-
-</body>
-</html> -->
