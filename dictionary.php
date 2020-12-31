f40bc4d9eaa2a1ac17c536cec477920f14b25d14
Imran-Nazir-485 <imrnazir8@gmail.com>
2020-12-31 12:31:04 +0500

    started work on my paperwork dictionary

---
+++ dictionary.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>My_Raw_Dictionary</title>
+</head>
+<style type="text/css">
+	*{margin: 0;padding: 0;}
+	main{margin: 14% 30%;}
+</style>
+<body>
+
+
+<main>
+	<center>
+<form method="post">
+	
+<label>Enter Word</label>
+<input type="text" name="word" placeholder="TypeHere...">
+<input type="submit" name="submit">
+
+
+</form>	
+		
+	</center>
+</main>
+
+
+
+
+</body>
+</html>
