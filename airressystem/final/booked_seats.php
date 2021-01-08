841fcd6e6a623ad6967a232901573d72031026cc
Imran-Nazir-485 <imrnazir8@gmail.com>
2021-01-08 14:18:01 +0500

    Solved problem of Seats

--- airressystem/final/booked_seats.php
+++ airressystem/final/booked_seats.php
@@ -34,6 +34,12 @@ h3{text-align: center;}
 <body>
 	<center>
 <br>
+<form method="POST"  action="admin1.php">
+	
+<input  type="submit"    name="t" value="tickets"  >
+
+</form>
+<br>
 	</center>
 <main>
 <h3 >Select Desired Seats</h3>
@@ -43,43 +49,104 @@ h3{text-align: center;}
 <br><br>
  <?php
 // // 1- Connect to database
- $myconn= mysqli_connect("localhost","root","","test");
+ $myconn= mysqli_connect("localhost","root","","airressystem");
 $rem=0;
 $ar = array();
-$myquery = "SELECT * FROM student";
+$arr = array();
+$myquery = "SELECT * FROM reservations";
 //3- Run Query
 $myresult = mysqli_query($myconn,$myquery);
 // $myrow = mysqli_fetch_assoc($myresult);
 while($myrow = mysqli_fetch_assoc($myresult))
 {
 
-array_push($ar,$myrow['seatnumber']);
+if ($myrow['rid']=='pk2300') {
+
+if ($myrow['status']=='ND') {
+array_push($ar,$myrow['seatnumber']);	
+}
+if ($myrow['status']=="Booked") {
+array_push($arr,$myrow['seatnumber']);	
+}
+
+
+}
+
+
+
+
+
 //$rem=$rem+1;
 }
 
-foreach($ar as $value){
-    echo $value . "<br>";
-}
-$myquery = "SELECT * FROM student";
+// foreach($arr as $value){
+//     echo $value . "<br>";
+// }
+$myquery = "SELECT * FROM flightschedule";
 $myresult = mysqli_query($myconn,$myquery);
-$rem=0;
-$myrow = mysqli_fetch_assoc($myresult);
+while ($myrow = mysqli_fetch_assoc($myresult)) {
+
+if ($myrow['fid']=='pk2300') {
+	$tbseats=$myrow['tbseats'];
+	$teseats=$myrow['teseats'];
+
+
+
+}
+
+
+}
+
+
+
+
+
+
+
+
+
+
+$rem=1;
 echo "<form method=\"POST\" action=\"\"  >";
 // onsubmit = function() { location. reload(true); }
-while($rem<=30)
+while($rem<=$tbseats)
 {
 
- if (in_array("B$rem", $ar)) 
-   { 
 
-echo "<input  type=\"button\"       value=\"B$rem\"      class=\"featuredBtn\" id=\"btn\" style=\"width: 50px ; background-color:red \"  >
+
+
+
+if ($rem%5==0) {
+	echo "<br>";
+}
+
+
+
+
+
+ if (in_array("BUS$rem", $ar)) 
+   { 
+// echo "This is called";
+echo "<input  type=\"submit\"    name=\"seat\"     value=\"BUS$rem\"      class=\"featuredBtn\" id=\"btn\" style=\"width: 50px ; background-color:green \"  >
 "."&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp";
 $rem++;
 }
-else
-{
 
-echo "<input  type=\"submit\"   name=\"seat\"    value=\"B$rem\"      class=\"featuredBtn\" id=\"btn\" style=\"width: 50px\"     >"."&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp";
+if (in_array("BUS$rem", $arr)) 
+   { 
+
+
+// echo "This is called";
+
+echo "<input  type=\"button\"       value=\"BUS$rem\"      class=\"featuredBtn\" id=\"btn\" style=\"width: 50px ; background-color:red \"  >
+"."&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp";
+$rem++;
+}
+if (!in_array("BUS$rem", $arr) && !in_array("BUS$rem", $ar)) 
+
+{
+// echo "This is called";
+echo "<input  type=\"submit\"   name=\"seat\"    value=\"BUS$rem\"      class=\"featuredBtn\" id=\"btn\" style=\"width: 50px\"     >"."&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp";
 $rem++;
 
 
@@ -101,6 +168,96 @@ $rem++;
 }
 echo "</form>";
 
+
+
+
+
+
+
+
+echo "<br><br>";
+
+
+
+
+
+
+
+
+
+$rem=1;
+echo "<form method=\"POST\" action=\"\"  >";
+// onsubmit = function() { location. reload(true); }
+while($rem<=$teseats)
+{
+
+
+
+
+
+if ($rem%5==0) {
+	echo "<br>";
+}
+
+
+
+
+
+ if (in_array("ECO$rem", $ar)) 
+   { 
+// echo "This is called";
+echo "<input  type=\"submit\"    name=\"seat\"     value=\"ECO$rem\"      class=\"featuredBtn\" id=\"btn\" style=\"width: 50px ; background-color:green \"  >
+"."&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp";
+$rem++;
+}
+
+if (in_array("ECO$rem", $arr)) 
+   { 
+
+
+// echo "This is called";
+
+echo "<input  type=\"button\"       value=\"ECO$rem\"      class=\"featuredBtn\" id=\"btn\" style=\"width: 50px ; background-color:red \"  >
+"."&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp";
+$rem++;
+}
+if (!in_array("ECO$rem", $arr) && !in_array("ECO$rem", $ar)) 
+
+{
+// echo "This is called";
+echo "<input  type=\"submit\"   name=\"seat\"    value=\"ECO$rem\"      class=\"featuredBtn\" id=\"btn\" style=\"width: 50px\"     >"."&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp";
+$rem++;
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+echo "</form>";
+
+
+
+
+
+
+
+
+
+
+
+
 ?>
 </main>
 
@@ -117,39 +274,50 @@ echo "</form>";
 
 
 
-
+ 
 <?php
 
 
 $count=0;
 
-$rem=0;
-
-
-
+$rem=1;
 if (isset($_POST['seat'])) {
 
 $seat=$_POST['seat'];
 echo "$seat";
 
-
-while(30)
+while($rem<=$tbseats  && $rem<=$teseats)
 {
 
  if (in_array($seat, $ar)) 
    { 
 
-echo "Same button is seleted twice"."<br>";
+//echo "Same button is seleted twice"."<br>";
 $count=1;
 break;
+
+}
+$rem++;
 }
 
 
-}
 
 
 if ($count==1) {
 	echo "Here We have To Delete The Record";
+
+
+
+$insertquery="DELETE FROM `reservations` WHERE seatnumber='$seat'";
+$res=mysqli_query($myconn,$insertquery);
+echo "Deleted";
+
+echo "<script type='text/javascript'>
+        window.location=document.location.href;
+        </script>";
+
+
+
 }
 
 if ($count==0) {
@@ -157,8 +325,11 @@ if ($count==0) {
 
 
 
+// INSERT INTO `reservations`(`bid`, `rid`, `depcity`, `destcity`, `date`, `time`, `numoftickets`, `seatnumber`, `cabin`, `status`) VALUES ([value-1],[value-2],[value-3],[value-4],[value-5],[value-6],[value-7],[value-8],[value-9],[value-10])
 
-$insertquery="INSERT INTO `student`(`seatnumber`) VALUES ('$seat')";
+
+
+$insertquery="INSERT INTO `reservations`( `rid`,  `seatnumber`, `status`) VALUES ('pk2300','$seat','ND')";
 $res=mysqli_query($myconn,$insertquery);
 echo "Inserted";
 // header("booked_seats.php");
@@ -167,8 +338,18 @@ echo "<script type='text/javascript'>
         </script>";
 }
 
+$a=sizeof($ar);
 
 
+// echo "$a";
+
+if (isset($_POST['t'])) {
+
+
+// if ($a==3) {
+	header("Location:admin1.php");
+// }
+}
 
 
 }
@@ -198,7 +379,6 @@ echo "<script type='text/javascript'>
 
 
 ?>
-
 
 
 
