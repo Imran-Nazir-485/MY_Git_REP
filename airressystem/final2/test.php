2ddda5cdbb2c3097b543e95af95b186dd98b5ce3
Imran-Nazir-485 <imrnazir8@gmail.com>
2021-01-16 10:44:25 +0500

    problem is solved

--- airressystem/final2/test.php
+++ airressystem/final2/test.php
@@ -6,12 +6,64 @@ include 'dbconn.php';
 
 
 
-$myquery = "SELECT * FROM routes t, flightschedule c WHERE 't.rid' = 'c.rid'";
+
+
+
+
+
+// $date = date('Y/m/d ');
+
+// echo "$date";
+
+
+
+
+
+$myquery = "SELECT * FROM  flightschedule ";
 
 $myresult = mysqli_query($myconn,$myquery);
 while($myrow = mysqli_fetch_assoc($myresult)){
-echo "this is called";
+
+$date = date("Y-m-d");
+$rid=$myrow['rid'];
+
+$myquery1 = "SELECT * FROM  routes WHERE rid='$rid'";
+$myresult1 = mysqli_query($myconn,$myquery1);
+$myrow1 = mysqli_fetch_assoc($myresult1);
+if ($myrow['rid']==$myrow1['rid'] && $myrow['depdate']>=$date) {
+echo $myrow['depdate']."<br>";
 	echo $myrow['rid']."<br>";
 }
 
+
+
+
+else{
+
+
+
+$flightstatus='Departured';
+
+$myquery2 = "UPDATE flightschedule SET flightstatus='$flightstatus' WHERE rid='$rid'";
+
+$myresult2 = mysqli_query($myconn,$myquery2);
+
+
+}
+}
+
 ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
